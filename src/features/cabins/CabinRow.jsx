40880cdffa4a4453e0e3788cf521c5ba91329d88
Akshansh Shrivastava <akshansh773@gmail.com>
2023-07-21 23:33:40 +0530

    Used Render Props pattern and Table Compound Component

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,17 +3,6 @@ import ConfirmDelete from "../../ui/ConfirmDelete";
 import { formatCurrency } from "../../utils/helpers";
 import useDeleteCabin from "./useDeleteCabin";
 import { HiTrash, HiPencil, HiDocumentDuplicate } from "react-icons/hi";
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
 
 const Img = styled.img`
   display: block;
@@ -21,7 +10,7 @@ const Img = styled.img`
   aspect-ratio: 3 / 2;
   object-fit: cover;
   object-position: center;
-  transform: scale(1.5) translateX(-7px);
+  transform: scale(1.5) translateX(1px);
 `;
 
 const Cabin = styled.div`
@@ -46,6 +35,7 @@ import React, { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import useCreateCabin from "./useCreateCabin";
 import Modal from "../../ui/Modal";
+import Table from "../../ui/Table";
 
 const CabinRow = ({ cabin }) => {
   const [showEditForm, setShowEditForm] = useState(false);
@@ -73,7 +63,7 @@ const CabinRow = ({ cabin }) => {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row>
         <Img src={image_url} />
         <Cabin>{name}</Cabin>
         <div>Fits up to {max_capacity} guests</div>
@@ -111,7 +101,7 @@ const CabinRow = ({ cabin }) => {
             </button>
           </Modal>
         </div>
-      </TableRow>
+      </Table.Row>
     </>
   );
 };
